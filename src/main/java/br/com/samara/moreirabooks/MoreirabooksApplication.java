a0da96064ecb4f65007fa58bea7f46ec50d14c88
SamaraMoreira <samaramoreira000@gmail.com>
2024-03-25 10:09:14 -0300

    Criação do projeto

---
+++ src/main/java/br/com/samara/moreirabooks/MoreirabooksApplication.java
@@ -0,0 +1,13 @@
+package br.com.samara.moreirabooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreirabooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoreirabooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/samara/moreirabooks/MoreirabooksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.samara.moreirabooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreirabooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
